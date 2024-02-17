883aab5b1ab53018d9de196c8244bb372aed38ae
Anurag <sahuanuragdev@gmail.com>
2024-02-17 23:50:48 +0530

    [feat] Changes in the workspace so that the new package (common types) works in that workspace

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,9 +3,14 @@ import Navbar from "@repo/ui/navbar";
 import { getServerSession } from "next-auth";
 import { GET } from "./api/auth/[...nextauth]/route";
 import styles from "./page.module.css";
+import { UserSessionType } from "common";
 
+type UserSessionResponseType = {
+  user: UserSessionType;
+  expires: string;
+} | null;
 export default async function Page(): Promise<JSX.Element> {
-  const session = await getServerSession(GET);
+  const session: UserSessionResponseType = await getServerSession(GET);
 
   return (
     <main className={styles.main}>
